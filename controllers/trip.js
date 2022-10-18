c1b26e691c1bc79b74bc6152b0d2e1f42d00184c
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-18 14:19:11 +0100

    trip crud

---
+++ controllers/trip.js
@@ -0,0 +1,70 @@
+const tripService = require("../services/trip");
+const AppError = require("../helpers/appError");
+module.exports = class trip {
+  static async apiGetAlltrips(req, res, next) {
+    try {
+      const trips = await tripService.getAlltrips(); 
+      if (!trips) {
+        res.status(404).json("There are no trip found yet!");
+      }
+      res.json(trips);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiGettripById(req, res, next) { 
+    try {
+      let id = req.params.id || {};
+      const trip = await tripService.gettripbyId(id);
+      res.json(trip);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+  static async apiChecktrip(req, res, next) { 
+    try {
+      const trip = await tripService.checktrip(req.body);
+       res.json(trip);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiCreatetrip(req, res, next) {
+    try {
+      console.log(req.body);
+      if (!req.body) return next(new AppError("No form data found", 404));
+      const createdtrip =  await tripService.createtrip(req.body);
+      res.json(createdtrip);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiUpdatetrip(req, res, next) {
+    try {
+     
+
+      const updatedtrip = await tripService.updatetrip(req.params.id,req.body.depart_city,req.body.arrive_city,req.body.depart_date,req.body.arrive_date,req.body.price,req.body.id_bus);
+
+      if (updatedtrip.modifiedCount === 0) {
+        throw new Error("Unable to update trip, error occord");
+      }
+
+      res.json(updatedtrip);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiDeletetrip(req, res, next) {
+    try {
+      const tripId = req.params.id;
+      const deleteResponse = await tripService.deletetrip(tripId);
+      res.json(deleteResponse);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+};
---
+++ services/trip.js
@@ -0,0 +1,74 @@
+const trip = require("../controllers/trip");
+const trip = require("../models/trip");
+
+module.exports = class tripService{
+    static async getAlltrips(){
+        try {
+            const alltrips = await trip.find();
+            return alltrips;
+        } catch (error) {
+            console.log(`Could not fetch trips ${error}`)
+        }
+    } 
+
+    static async createtrip(data){
+        try {
+            
+            const newtrip = {
+                depart_city: data.depart_city,  
+                arrive_city: data.arrive_city,  
+                depart_date: data.depart_date,
+                arrive_city: data.arrive_city,
+                price: data.price,
+                id_bus: data.id_bus
+
+            }
+           const response = await new trip(newtrip).save();
+           return response;
+        } catch (error) {
+            console.log(error);
+        } 
+
+    }
+    static async checktrip(data){
+        try {
+            
+            const checktrip =  await trip.find({depart_city: data.depart_city,arrive_city: data.arrive_city,});
+            
+            return checktrip;
+        } catch (error) {
+            console.log(`trip not found. ${error}`)
+        }
+    }
+
+    static async gettripbyId(tripId){
+        try {
+            const singletripResponse =  await trip.findById({_id: tripId});
+            return singletripResponse;
+        } catch (error) {
+            console.log(`trip not found. ${error}`)
+        }
+    }
+
+    static async updatetrip(tripId,depart_city,arrive_city,depart_date,arrive_date,price,id_bus){
+            try {
+                const updateResponse =  await trip.findByIdAndUpdate({_id:tripId},
+                    {depart_city, arrive_city,depart_date,arrive_date,price,id_bus});
+
+                    return updateResponse;
+            } catch (error) {
+                console.log(`Could not update trip ${error}` );
+
+        }
+    }
+
+    static async deletetrip(tripId){
+        try {
+            const deletedResponse = await trip.findByIdAndDelete(tripId);
+            return deletedResponse;
+        } catch (error) {
+            console.log(`Could not delete trip ${error}`);
+        }
+
+    }
+}
