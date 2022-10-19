dbfc4d483195f8646a5e2e058fee1eadefbda541
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-19 09:54:41 +0100

    ctrl and srvc

---
+++ controllers/reservation.js
@@ -0,0 +1,66 @@
+const reservationService = require("../services/reservation");
+const AppError = require("../helpers/appError");
+module.exports = class reservation {
+  static async apiGetAllreservations(req, res, next) {
+    try {
+      const reservations = await reservationService.getAllreservations(); 
+      if (!reservations) {
+        res.status(404).json("There are no reservation found yet!");
+      }
+      res.json(reservations);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiGetreservationById(req, res, next) { 
+    try {
+      let id = req.params.id || {};
+      const reservation = await reservationService.getreservationbyId(id);
+      res.json(reservation);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+  static async apiCheckreservation(req, res, next) { 
+    try {
+      const reservation = await reservationService.checkreservation(req.body);
+       res.json(reservation);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiCreatereservation(req, res, next) {
+    try {
+      console.log(req.body);
+      if (!req.body) return next(new AppError("No form data found", 404));
+      const createdreservation =  await reservationService.createreservation(req.body);
+      res.json(createdreservation);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiUpdatereservation(req, res, next) {
+    try {
+      const updatedreservation = await reservationService.updatereservation(req.params.id,req.body.ticket_number,req.body.price,req.body.id_owner,req.body.id_trip,req.body.id_bus);
+      if (updatedreservation.modifiedCount === 0) {
+        throw new Error("Unable to update reservation, error occord");
+      }
+      res.json(updatedreservation);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+
+  static async apiDeletereservation(req, res, next) {
+    try {
+      const reservationId = req.params.id;
+      const deleteResponse = await reservationService.deletereservation(reservationId);
+      res.json(deleteResponse);
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+};
---
+++ services/reservation.js
@@ -0,0 +1,70 @@
+const reservation = require("../models/reservation");
+
+module.exports = class reservationService{
+    static async getAllreservations(){
+        try {
+            const allreservations = await reservation.find();
+            return allreservations;
+        } catch (error) {
+            console.log(`Could not fetch reservations ${error}`)
+        }
+    } 
+
+    static async createreservation(data){
+        try {
+            
+            const newreservation = {
+                ticket_number: data.ticket_number, 
+                price: data.price,
+                id_owner: data.id_owner, 
+                id_trip: data.id_trip,
+                id_bus: data.id_bus
+
+            }
+           const response = await new reservation(newreservation).save();
+           return response;
+        } catch (error) {
+            console.log(error);
+        } 
+
+    }
+    static async checkreservation(data){
+        try {      
+            const checkreservation =  await reservation.find({id_owner: data.id_owner,id_trip: data.id_trip,});    
+            return checkreservation;
+        } catch (error) {
+            console.log(`reservation not found. ${error}`)
+        }
+    }
+
+    static async getreservationbyId(reservationId){
+        try {
+            const singlereservationResponse =  await reservation.findById({_id: reservationId});
+            return singlereservationResponse;
+        } catch (error) {
+            console.log(`reservation not found. ${error}`)
+        }
+    }
+
+    static async updatereservation(reservationId,ticket_number,price,id_owner,id_trip,id_bus){
+            try {
+                const updateResponse =  await reservation.findByIdAndUpdate({_id:reservationId},
+                    {ticket_number,price,id_owner,id_trip,id_bus});
+
+                    return updateResponse;
+            } catch (error) {
+                console.log(`Could not update reservation ${error}` );
+
+        }
+    }
+
+    static async deletereservation(reservationId){
+        try {
+            const deletedResponse = await reservation.findByIdAndDelete(reservationId);
+            return deletedResponse;
+        } catch (error) {
+            console.log(`Could not delete reservation ${error}`);
+        }
+
+    }
+}
