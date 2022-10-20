799c8d148d712334db33612c3168aa5330ed7234
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-20 15:06:04 +0100

    fntawsm icons

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,25 @@
+
+
 const navbar = () => {
     return(
-     <div>
+    <>
+     <div className="bg-cyan-600 p-5 flex justify-around">
          <div>
-             <img src="" />
+             <img src={`${process.env.PUBLIC_URL}/images/TravelBook.svg`}  />
+         </div>
+         <div className="text-white font-bold">
+             <span><i class="fa fa-envelope" aria-hidden="true"></i> kamalghrabla@gmail.com</span>
+             <span className="ml-8"><i class="fa-solid fa-phone " ></i> 0625467800</span>
          </div>
      </div>
+     <div className="flex justify-center gap-40 m-4 font-bold uppercase">
+      <a href="#"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+      <a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a>
+      <a href="#"><i class="fa-solid fa-registered"></i> Register</a>
+      <a href="#"><i class="fa-solid fa-right-from-bracket"></i> Logout</a>
+      <a href="#"><i class="fa-solid fa-cart-shopping"></i> Panier</a>
+     </div>
+     </>
     )
 }
 
