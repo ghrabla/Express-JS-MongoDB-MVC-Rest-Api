cb1dc6dcee967f31d31302d822d4f334dcc6286f
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-22 13:53:45 +0100

    back back

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Offer from "./components/offer";
+import Service from "./components/service";
+import Footer from "./components/footer";
 import Login from "./components/Login";
 
 function App() {
@@ -7,12 +12,16 @@ function App() {
     <>
     <BrowserRouter>
    
-      <div className="App">
         <Routes>
         <Route path='/Login' element={<Login/>}/>
           {/* <Route /> */}
         </Routes>
-     
+        <div className="App">
+      <Navbar/>
+      <Header/>
+      <Offer/>
+      <Service/>
+      <Footer/>
     </div>
       </BrowserRouter>
     </>
