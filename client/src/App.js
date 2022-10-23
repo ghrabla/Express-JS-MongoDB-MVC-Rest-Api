177c08c8ce1f02fbfbb26ac198620e72bccf8662
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-23 19:38:56 +0100

    all pages routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Footer from "./pages/components/footer";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Trip from "./pages/Trip";
+import Bus from "./pages/Bus";
+import User from "./pages/User";
+import Reservation from "./pages/Reservation";
 import Loginadmin from "./pages/Loginadmin";
 import Home from "./pages/Home";
 
@@ -19,6 +22,9 @@ function App() {
           <Route path='/Loginadmin' element={<Loginadmin/>}/>
           <Route path='/Register' element={<Register/>}/>
           <Route path='/Trip' element={<Trip/>}/>
+          <Route path='/Bus' element={<Bus/>}/>
+          <Route path='/User' element={<User/>}/>
+          <Route path='/Reservation' element={<Reservation/>}/>
         </Routes>
       <Footer/>
     </div>
