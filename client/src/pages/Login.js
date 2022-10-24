2d6f66a04c0f01d60bca46128278283e0f34224e
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-24 09:52:10 +0100

    login edit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -44,12 +44,12 @@ const login = () => {
                             </div>
                         </form>
                         <div className="mt-12 text-sm font-display font-semibold text-cyan-600 text-center">
-                            you don't have an account ?  <a className="cursor-pointer text-cyan-600 hover:text-indigo-800">Register</a> 
+                            you don't have an account ?  <a className="cursor-pointer text-black hover:text-indigo-800">Register</a> 
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="hidden lg:flex items-center justify-center bg-indigo-100 flex-1 h-screen">
+            <div className="hidden lg:flex items-center justify-center bg-gray-200 rounded flex-1 h-screen">
                 <div className="max-w-xs transform duration-200 hover:scale-110 cursor-pointer">
                     <svg className="w-5/6 mx-auto" xmlns="http://www.w3.org/2000/svg" id="f080dbb7-9b2b-439b-a118-60b91c514f72" data-name="Layer 1" viewBox="0 0 528.71721 699.76785">
                         <title>Login</title>
