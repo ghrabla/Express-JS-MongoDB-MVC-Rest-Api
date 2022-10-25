f2b566004ce1c75c8cbf7f3f23dd7b083baa6c38
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-25 17:08:52 +0100

    redux coding

---
+++ client/src/actions/types.js
@@ -0,0 +1,5 @@
+export const CREATE_TRIP = "CREATE_TRIP";
+export const RETRIEVE_TRIPS = "RETRIEVE_TRIPS";
+export const UPDATE_TRIP = "UPDATE_TRIP";
+export const DELETE_TRIP = "DELETE_TRIP";
+export const DELETE_ALL_TRIPS = "DELETE_ALL_TRIPS";
---
+++ client/src/services/tripService.js
@@ -0,0 +1,41 @@
+import http from "../http-common";
+
+const getAll = () => {
+  return http.get("/trip");
+};
+
+const get = id => {
+  return http.get(`/trip/${id}`);
+};
+
+const create = data => {
+  return http.post("/trip", data);
+};
+
+const update = (id, data) => {
+  return http.put(`/trip/${id}`, data);
+};
+
+const remove = id => {
+  return http.delete(`/trip/${id}`);
+};
+
+const removeAll = () => {
+  return http.delete(`/trip`);
+};
+
+const findtrip = data => {
+  return http.post(`/trip/check`,data);
+};
+
+const tripService = {
+  getAll,
+  get,
+  create,
+  update,
+  remove,
+  removeAll,
+  findtrip
+};
+
+export default tripService;
