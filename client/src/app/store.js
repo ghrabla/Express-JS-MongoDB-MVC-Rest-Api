2a3edfe6d74db377c1c47ead3786a760d6942954
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-31 01:10:45 +0100

    auth admin

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
+import authAdminReducer from '../features/authAdmin/authSlice'
 import tripReducer from '../features/trips/tripSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    authAdmin: authAdminReducer,
     trips: tripReducer,
+    
   },
 })
