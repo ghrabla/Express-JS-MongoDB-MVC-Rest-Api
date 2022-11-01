1f14e9898f6d121d7874fb4d4d410504de66dd96
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-11-01 15:04:18 +0100

    let's fix the bug of trip html

--- client/src/pages/Trip.js
+++ client/src/pages/Trip.js
@@ -70,88 +70,94 @@ const Trip = () => {
                 </button>
               </p>
             </div>
-            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-              <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
-                <table class="min-w-full leading-normal">
-                  <thead>
-                    <tr>
-                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                        Depart/Arrive city
-                      </th>
-                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                        Price
-                      </th>
-                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                        Depart/Arrive date
-                      </th>
-                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                        Bus
-                      </th>
-                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr v-for="product in products">
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <div class="flex">
-                          <div class="ml-3">
-                            <p class="text-red-900 whitespace-no-wrap ">Safi</p>
-                            <p class="text-green-700 whitespace-no-wrap font-bold">
-                              Rabat
+            <div>
+             <div>
+               {trips.map((one)=>(
+                  <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                  <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
+                    <table class="min-w-full leading-normal">
+                      <thead>
+                        <tr>
+                          <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                            Depart/Arrive city
+                          </th>
+                          <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                            Price
+                          </th>
+                          <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                            Depart/Arrive date
+                          </th>
+                          <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                            Bus
+                          </th>
+                          <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="product in products">
+                          <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <div class="flex">
+                              <div class="ml-3">
+                                <p class="text-red-900 whitespace-no-wrap ">Safi</p>
+                                <p class="text-green-700 whitespace-no-wrap font-bold">
+                                  Rabat
+                                </p>
+                              </div>
+                            </div>
+                          </td>
+                          <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-gray-900 whitespace-no-wrap">55.00</p>
+                            <p class="text-gray-600 whitespace-no-wrap">DH</p>
+                          </td>
+                          <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p class="text-red-900 whitespace-no-wrap">
+                              09/12/2022
                             </p>
-                          </div>
-                        </div>
-                      </td>
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p class="text-gray-900 whitespace-no-wrap">55.00</p>
-                        <p class="text-gray-600 whitespace-no-wrap">DH</p>
-                      </td>
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p class="text-red-900 whitespace-no-wrap">
-                          09/12/2022
-                        </p>
-                        <p class="text-green-600 whitespace-no-wrap font-bold">
-                          11/12/2022
-                        </p>
-                      </td>
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                          <span
-                            aria-hidden
-                            class="absolute inset-0 opacity-50 rounded-full"
-                          ></span>
-                          <span class="relative">ACHKID</span>
-                        </span>
-                      </td>
-                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                        <div className={action ? "hidden" : "block"}>
-                        <button
-                          onClick={showaction}
-                          type="button"
-                          class="inline-block text-gray-500 hover:text-gray-700"
-                        >
-                          <svg
-                            class="inline-block h-6 w-6 fill-current"
-                            viewBox="0 0 24 24"
-                          >
-                            <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
-                          </svg>
-                        </button>
-                        </div>
-                        <div className={action ? "block" : "hidden"}>
-                        <button onClick={showaction} class="font-bold text-xl" ><i class="fa-solid fa-xmark"></i></button>
-                        </div>
-                      </td>
-                      <div className={action ? "block" : "hidden"}>
-                      <div class="flex flex-col gap-3" >
-                  <button  class="text-green-500 font-bold" onClick={funshowupdate}><i class="fas fa-edit" ></i>Update</button>
-                  <button class="text-red-500 font-bold" ><i class="fa fa-trash" aria-hidden="true"></i>Delete</button>
-              </div> 
-              </div>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+                            <p class="text-green-600 whitespace-no-wrap font-bold">
+                              11/12/2022
+                            </p>
+                          </td>
+                          <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                              <span
+                                aria-hidden
+                                class="absolute inset-0 opacity-50 rounded-full"
+                              ></span>
+                              <span class="relative">ACHKID</span>
+                            </span>
+                          </td>
+                          <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+                            <div className={action ? "hidden" : "block"}>
+                            <button
+                              onClick={showaction}
+                              type="button"
+                              class="inline-block text-gray-500 hover:text-gray-700"
+                            >
+                              <svg
+                                class="inline-block h-6 w-6 fill-current"
+                                viewBox="0 0 24 24"
+                              >
+                                <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
+                              </svg>
+                            </button>
+                            </div>
+                            <div className={action ? "block" : "hidden"}>
+                            <button onClick={showaction} class="font-bold text-xl" ><i class="fa-solid fa-xmark"></i></button>
+                            </div>
+                          </td>
+                          <div className={action ? "block" : "hidden"}>
+                          <div class="flex flex-col gap-3" >
+                      <button  class="text-green-500 font-bold" onClick={funshowupdate}><i class="fas fa-edit" ></i>Update</button>
+                      <button class="text-red-500 font-bold" ><i class="fa fa-trash" aria-hidden="true"></i>Delete</button>
+                  </div> 
+                  </div>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+               ))}
+             </div>
             </div>
           </div>
         </div>
